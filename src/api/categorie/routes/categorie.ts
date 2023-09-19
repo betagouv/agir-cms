6ca0f9ad4b01589f31a723c71147a97cf54c6649
Denis B <denis.baudot@gmail.com>
2023-09-19 14:16:36 +0200

    remove old thematique type

--- src/api/categorie/routes/categorie.ts
+++
@@ -1,7 +0,0 @@
-/**
- * categorie router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::categorie.categorie');
--- src/api/categorie/services/categorie.ts
+++
@@ -1,7 +0,0 @@
-/**
- * categorie service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::categorie.categorie');
