b048a93a511c82cbe04607aab18c64213b8fcd2c
Denis B <denis.baudot@gmail.com>
2023-10-06 14:40:28 +0200

    fix publish/unpublish bug

--- src/api/article/content-types/article/lifecycles.ts
+++ src/api/article/content-types/article/lifecycles.ts
@@ -6,17 +6,19 @@ export default {
     const missingRelations = await strapi
       .service("api::custom.validation")
       .checkRequiredRelationsCreate(event);
-    if (missingRelations.length)
+    if (missingRelations.length > 0)
       throw new ApplicationError(
         "Il manque une ou des relation(s) : " + missingRelations.join(", ")
       );
   },
   async beforeUpdate(event) {
+    if (!event.params.data.id) return;
+
     const missingRelations = await strapi
       .service("api::custom.validation")
       .checkRequiredRelationsUpdate(event);
 
-    if (missingRelations.length)
+    if (missingRelations.length > 0)
       throw new ApplicationError(
         "Il manque une ou des relation(s) : " + missingRelations.join(", ")
       );
