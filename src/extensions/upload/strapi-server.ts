be50da6958620558c4fecaab2cbcfd197515cbf9
Denis B <denis.baudot@gmail.com>
2023-10-17 14:13:06 +0200

    more cleaninig

--- src/extensions/upload/strapi-server.ts
+++ src/extensions/upload/strapi-server.ts
@@ -1,10 +1,3 @@
-const getService = (name) => {
-  return strapi.plugin("upload").service(name);
-};
-
-const axios = require("axios");
-const imageSize = require("image-size");
-
 module.exports = (plugin) => {
   plugin.services.upload = require("./custom-upload.js");
   return plugin;
