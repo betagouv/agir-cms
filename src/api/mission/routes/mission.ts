b0bed91970e9fd6551a0fdb5e1c00021410f3119
Wojciech WOJCIK <wwojcik@octo.com>
2024-05-22 16:33:34 +0200

    ajout de l'objet mission-objectifs

---
+++ src/api/mission/routes/mission.ts
@@ -0,0 +1,7 @@
+/**
+ * mission router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::mission.mission');
---
+++ src/api/mission/services/mission.ts
@@ -0,0 +1,7 @@
+/**
+ * mission service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::mission.mission');
