f3d1666b19a2138ea4d0d1debb3296bd9d070868
Wojciech WOJCIK <wwojcik@octo.com>
2024-05-22 16:54:45 +0200

    ajout KYC au modèle

---
+++ src/api/kyc/controllers/kyc.ts
@@ -0,0 +1,7 @@
+/**
+ * kyc controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::kyc.kyc');
---
+++ src/api/kyc/routes/kyc.ts
@@ -0,0 +1,7 @@
+/**
+ * kyc router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::kyc.kyc');
