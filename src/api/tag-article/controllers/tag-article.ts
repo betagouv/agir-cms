0ba25210107091846aaa93a99087f846c4db621e
Wojciech WOJCIK <wwojcik@octo.com>
2024-07-03 16:21:19 +0200

    ajout tag des articles pour regroupement dans missions

---
+++ src/api/tag-article/controllers/tag-article.ts
@@ -0,0 +1,7 @@
+/**
+ * tag-article controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::tag-article.tag-article');
---
+++ src/api/tag-article/services/tag-article.ts
@@ -0,0 +1,7 @@
+/**
+ * tag-article service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::tag-article.tag-article');
