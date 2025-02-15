495c775cc8ade42acad651b9bae4c49ec8b5faaa
Wojciech WOJCIK <wojciech.wojcik@gmail.com>
2025-02-15 12:15:25 +0100

    suppression action historique all in one

--- src/api/action/controllers/action.ts
+++
@@ -1,7 +0,0 @@
-/**
- * action controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::action.action');
--- src/api/action/services/action.ts
+++
@@ -1,7 +0,0 @@
-/**
- * action service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::action.action');
