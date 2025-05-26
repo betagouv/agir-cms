c3253b96e23cc5e9e3b3f3dac37b07b44fe938aa
Wojciech WOJCIK <wojciech.wojcik@gmail.com>
2025-05-26 11:57:24 +0200

    suppression de la collection tag_article

--- src/api/tag-article/controllers/tag-article.ts
+++
@@ -1,7 +0,0 @@
-/**
- * tag-article controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::tag-article.tag-article');
--- src/api/tag-article/services/tag-article.ts
+++
@@ -1,7 +0,0 @@
-/**
- * tag-article service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::tag-article.tag-article');
