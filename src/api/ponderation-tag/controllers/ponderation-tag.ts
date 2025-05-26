8029d0d6e458373106efd9b1cc4019979d24eee5
Wojciech WOJCIK <wojciech.wojcik@gmail.com>
2025-05-26 16:39:02 +0200

    ajout ponderation de tags

---
+++ src/api/ponderation-tag/controllers/ponderation-tag.ts
@@ -0,0 +1,7 @@
+/**
+ * ponderation-tag controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::ponderation-tag.ponderation-tag');
---
+++ src/api/ponderation-tag/routes/ponderation-tag.ts
@@ -0,0 +1,7 @@
+/**
+ * ponderation-tag router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::ponderation-tag.ponderation-tag');
