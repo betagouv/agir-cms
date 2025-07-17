7cfe74574823934d3b6fca9932f213151207344d
Wojciech WOJCIK <wojciech.wojcik@gmail.com>
2025-07-17 17:15:43 +0200

    ajout collection 'selection'

---
+++ src/api/selection/routes/selection.ts
@@ -0,0 +1,7 @@
+/**
+ * selection router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::selection.selection');
---
+++ src/api/selection/services/selection.ts
@@ -0,0 +1,7 @@
+/**
+ * selection service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::selection.selection');
